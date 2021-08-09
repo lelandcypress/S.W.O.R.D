551f58b582d45dc605ab688521544ae12ca6eb01
Michael Souter <souterm1@gmail.com>
2021-08-09 13:55:47 -0400

    added missionRoutes.js which has POST and DELETE routes for our missions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const missionRoutes = require('./missionRoutes');
+
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/missions', missionRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,69 +5,62 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     // Get all missions and JOIN with user data
-    const currentMissionData = await Mission.findAll(
-      {
-        attributes: {
-          include: [
-            ['id','mission_id'],
-            ['name','mission_name'],
-            'location',
-            'date_created',
-            'priority',
-            'status'
-          ],
-        },
-        include: [{
-          model: Hero,
-          attributes: ['name']
-        }],
-        order: [
-          ['date_created', 'DESC'],
+    const currentMissionData = await Mission.findAll({
+      attributes: {
+        include: [
+          ['id', 'mission_id'],
+          ['name', 'mission_name'],
+          'location',
+          'date_created',
+          'priority',
+          'status',
         ],
-      }
-    );
+      },
+      include: [
+        {
+          model: Hero,
+          attributes: ['name'],
+        },
+      ],
+      order: [['date_created', 'DESC']],
+    });
 
     // Serialize data so the template can read it
-    const missions = currentMissionData.map(x => x.get({ plain: true }));
+    const missions = currentMissionData.map((x) => x.get({ plain: true }));
 
-    res
-      .status(200)
-      .render('homepage', { missions });
+    res.status(200).render('homepage', { missions });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/mission/:id', async (req, res) => {
   try {
     // Get specific mission
-    const selectedMissionData = await Mission.findByPk(
-      req.params.id,
-      {
-        attributes: {
-          include: [
-            ['id','mission_id'],
-            ['name','mission_name'],
-            'location',
-            'date_created',
-            'priority',
-            'status',
-            'description'
-          ],
-        },
-        include: [{
+    const selectedMissionData = await Mission.findByPk(req.params.id, {
+      attributes: {
+        include: [
+          ['id', 'mission_id'],
+          ['name', 'mission_name'],
+          'location',
+          'date_created',
+          'priority',
+          'status',
+          'description',
+        ],
+      },
+      include: [
+        {
           model: Hero,
-          attributes: ['name']
-        }],
-      }
-    );
+          attributes: ['name'],
+        },
+      ],
+    });
 
     // Serialize data so the template can read it
     const mission = selectedMissionData.get({ plain: true });
 
-    res
-      .status(200)
-      .render('homepage', mission);
+    res.status(200).render('homepage', mission);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -77,40 +70,41 @@ router.get('/project/:id', async (req, res) => {
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(
-      req.session.user_id, 
-      {
-        attributes: { exclude: ['password'] },
-        include: [{ 
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [
+        {
           model: Hero,
           attributes: [
-            ['name','hero_name'],
+            ['name', 'hero_name'],
             'secret_identity',
             'organization',
             'powers',
-            'weakness'
+            'weakness',
           ],
-          include: [{
-            model: Mission,
-            attributes: [
-              ['id','mission_id'],
-              ['name','mission_name'],
-              'location',
-              'date_created',
-              'priority',
-              'status',
-              'description'
-            ]
-          }]
-        }],
-      }
-    );
+          include: [
+            {
+              model: Mission,
+              attributes: [
+                ['id', 'mission_id'],
+                ['name', 'mission_name'],
+                'location',
+                'date_created',
+                'priority',
+                'status',
+                'description',
+              ],
+            },
+          ],
+        },
+      ],
+    });
 
     const user = userData.get({ plain: true });
 
     res.render('profile', {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
