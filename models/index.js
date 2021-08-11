17070d271056902c4373db2ff138c2a9a649b043
Esteban Santos <santosesteban09@gmail.com>
2021-08-10 21:19:42 -0500

    Homepage has been updated with basic functionality. Shows table with missions. Has create button.

--- models/index.js
+++ models/index.js
@@ -1,14 +1,17 @@
 const Hero = require('./Hero');
 const Mission = require('./Mission');
 const User = require('./User');
-
+User.belongsTo(Hero, {
+  foreignKey: 'hero_id',
+});
 Hero.hasOne(User, {
   foreignKey: 'hero_id',
   onDelete: 'CASCADE',
 });
-
 Mission.hasMany(Hero, {
   foreignKey: 'mission_id',
 });
-
+Hero.belongsTo(Mission, {
+  foreignKey: 'mission_id',
+});
 module.exports = { Hero, Mission, User };
