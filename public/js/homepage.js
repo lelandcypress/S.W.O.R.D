ba255b570dabebc514cb35d109c61739add77a59
Ryan Adams <ryanadams14@gmail.com>
2021-08-17 19:44:44 -0500

    fixing issue with links not opening on active missions

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,29 +1,30 @@
 // BUTTON TO SHOW UP WHEN NO HERO ASSIGNED. THIS WILL SEND UPDATE TO SERVER FOR HERO ASSIGNED
 const joinMission = async (event) => {
-    event.preventDefault();
-    // const join_mission = document.querySelector('#join_mission').value;
-     if(event.target.hasAttribute('data-id')){
-        const id = event.target.getAttribute('data-id');
-        // const hero = req.session.user;
-        const response = await fetch(`/api/missions/heroassign/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                id,
-            }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-          if (response.ok) {
-            document.location.replace(`/`);
-          } else {
-            document.location.replace(`/login`);
-          }
-        }
-    
-    
+  event.preventDefault();
+  console.log(event)
+  // const join_mission = document.querySelector('#join_mission').value;
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    // const hero = req.session.user;
+    const response = await fetch(`/api/missions/heroassign/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace(`/`);
+    } else {
+      document.location.replace(`/login`);
     }
-    
-    
-  
-document.querySelector('#missionTable').addEventListener('click', joinMission);
+  }
+}
+
+const joinBtn = document.querySelectorAll('.join_mission');
+console.log(joinBtn);
+if (joinBtn[0]) {
+  [...joinBtn].map(btn => btn.addEventListener('click', joinMission));
+}
