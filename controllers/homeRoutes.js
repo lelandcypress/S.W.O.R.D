cd7c3be3844484a1f55fa05ab11d380fa5a10f08
Ryan Adams <ryanadams14@gmail.com>
2021-08-18 19:59:19 -0500

    updated homeroutes and, you guessed it, css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const { Mission, Hero, User } = require('../models');
-const { findAll } = require('../models/Hero');
+const {
+  Mission,
+  Hero,
+  User
+} = require('../models');
+const {
+  findAll
+} = require('../models/Hero');
 const withAuth = require('../utils/auth');
 
 
@@ -30,24 +36,26 @@ router.get(['/', '/:id([0-9]{1,})'], async (req, res) => {
           'status',
         ],
       },
-      include: [
-        {
-          model: Hero,
-          attributes: ['name'],
-          required: false,
-          raw: true,
-          nest: true,
-        },
-      ],
+      include: [{
+        model: Hero,
+        attributes: ['name'],
+        required: false,
+        raw: true,
+        nest: true,
+      }, ],
       offset: offset,
       limit: 20,
-      order: [['date_created', 'DESC']],
+      order: [
+        ['date_created', 'DESC']
+      ],
     });
 
     let missions = [];
     // Serialize data so the template can read it
     currentMissionData
-      .map((x) => x.get({ plain: true }))
+      .map((x) => x.get({
+        plain: true
+      }))
       .map((x) => {
         missions.push({
           id: x.id,
@@ -75,35 +83,36 @@ router.get(['/', '/:id([0-9]{1,})'], async (req, res) => {
       if (!user_id) return false;
 
       const userData = await User.findByPk(user_id, {
-        attributes: { exclude: ['password', 'id', 'username', 'email'] },
-        include: [
-          {
-            model: Hero,
-            attributes: ['mission_id'],
-          },
-        ],
+        attributes: {
+          exclude: ['password', 'id', 'username', 'email']
+        },
+        include: [{
+          model: Hero,
+          attributes: ['mission_id'],
+        }, ],
       });
 
 
-      const user = userData.get({ plain:true });
+      const user = userData.get({
+        plain: true
+      });
       console.log("line 82 " + user.hero.mission_id);
       const heromisID = user.hero.mission_id;
-      console.log("Line 84 " +heromisID);
-      if (heromisID){
-        res.push({onMission: true});
-      } else{
-        res.push({onMission: false});
-      };
+      console.log("Line 84 " + heromisID);
+      // if (heromisID){
+      //   res.push({onMission: true});
+      // } else{
+      //   res.push({onMission: false});
+      // };
       console.log("Line 90 " + res)
     }
-    
+
 
 
     let pageLinks = [];
 
     if (page === 0) {
-      pageLinks = [
-        {
+      pageLinks = [{
           path: '/',
           display: 1,
         },
@@ -117,8 +126,7 @@ router.get(['/', '/:id([0-9]{1,})'], async (req, res) => {
         },
       ];
     } else {
-      pageLinks = [
-        {
+      pageLinks = [{
           path: `/${Number(page)-1}`,
           display: `${Number(page)}`,
         },
@@ -157,30 +165,30 @@ router.get(['/', '/:id([0-9]{1,})'], async (req, res) => {
     } else {
       renderPage(true);
     }
-//     const renderPage = async (res) => {
-//       console.log("line 94 " + res);
-//       join.status(200).render('homepage', {
-//         missions,
-//         logged_in: req.session.logged_in,
-//         canJoinNewMission: res.onMission
-//       });
-//     }
-//     canJoin(req.session.user_id);
-//     renderPage(res);
-    
+    //     const renderPage = async (res) => {
+    //       console.log("line 94 " + res);
+    //       join.status(200).render('homepage', {
+    //         missions,
+    //         logged_in: req.session.logged_in,
+    //         canJoinNewMission: res.onMission
+    //       });
+    //     }
+    //     canJoin(req.session.user_id);
+    //     renderPage(res);
 
-//     // if (canJoin(req.session.user_id)) {
-//     //   renderPage(false);
-//     // } else {
-//     //   renderPage(true);
-//     // }
+
+    //     // if (canJoin(req.session.user_id)) {
+    //     //   renderPage(false);
+    //     // } else {
+    //     //   renderPage(true);
+    //     // }
 
   } catch (err) {
-    join.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.get('/mission/:id', withAuth, async (req, join) => {
+router.get('/mission/:id', withAuth, async (req, res) => {
   try {
     // Get specific mission
     const selectedMissionData = await Mission.findByPk(req.params.id, {
@@ -195,112 +203,116 @@ router.get('/mission/:id', withAuth, async (req, join) => {
           'date_created',
         ],
       },
-      include: [
-        {
-          model: Hero,
-          attributes: ['name'],
-        },
-      ],
+      include: [{
+        model: Hero,
+        attributes: ['name'],
+      }, ],
     });
 
     // Serialize data so the template can read it
-    let mission = selectedMissionData.get({ plain: true });
+    let mission = selectedMissionData.get({
+      plain: true
+    });
     const heroname = mission.heros[0].name;
     mission.hero = heroname;
 
 
 
-    join.status(200).render('mission', {
+    res.status(200).render('mission', {
       ...mission,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
-    join.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, join) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     let listOfAvailableMissions, missions;
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [
-        {
-          model: Hero,
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model: Hero,
+        attributes: [
+          ['name', 'hero_name'],
+          'secret_identity',
+          'organization',
+          'powers',
+          'weakness',
+        ],
+        include: [{
+          model: Mission,
           attributes: [
-            ['name', 'hero_name'],
-            'secret_identity',
-            'organization',
-            'powers',
-            'weakness',
-          ],
-          include: [
-            {
-              model: Mission,
-              attributes: [
-                ['id', 'mission_id'],
-                ['name', 'mission_name'],
-                'location',
-                'date_created',
-                'priority',
-                'status',
-                'description',
-              ],
-            },
-          ],
-        },
-      ],
-    });
-
-    const user = userData.get({ plain: true });
-
-    // console.log(user.hero.mission);
-
-    if (!user.hero.mission) {
-      listOfAvailableMissions = await Mission.findAll({
-        attributes: {
-          include: [
             ['id', 'mission_id'],
             ['name', 'mission_name'],
             'location',
             'date_created',
             'priority',
             'status',
+            'description',
           ],
-        },
-        order: [['date_created', 'DESC']],
-      });
+        }, ],
+      }, ],
+    });
 
-      missions = listOfAvailableMissions.map((x) => x.get({ plain: true }));
-    }
+    const user = userData.get({
+      plain: true
+    });
 
-    join.render('profile', {
+    // console.log(user.hero.mission);
+
+    // if (!user.hero.mission || user.hero.mission) {
+    listOfAvailableMissions = await Mission.findAll({
+      attributes: {
+        include: [
+          ['id', 'mission_id'],
+          ['name', 'mission_name'],
+          'location',
+          'date_created',
+          'priority',
+          'status',
+        ],
+      },
+      order: [
+        ['date_created', 'DESC']
+      ],
+    });
+
+    missions = listOfAvailableMissions.map((x) => x.get({
+      plain: true
+    }));
+    // }
+
+    res.render('profile', {
       ...user,
       missions,
       logged_in: true,
     });
   } catch (err) {
-    join.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, join) => {
+router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    join.redirect('/profile');
+    res.redirect('/profile');
     return;
   }
 
-  join.render('login');
+  res.render('login');
 });
 
-router.get('/create', (req, join) => {
+router.get('/create', (req, res) => {
   // If the user is already logged in, redirect the request to another route
 
 
-  join.render('missionCreate',{
+  res.render('missionCreate', {
     logged_in: req.session.logged_in,
   });
 });
@@ -317,9 +329,13 @@ router.get('/roster', withAuth, async (req, res) => {
           'weakness',
         ],
       },
-      order: [['organization', 'DESC']],
+      order: [
+        ['organization', 'DESC']
+      ],
     });
-    let heroList = heroRoster.map((x) => x.get({ plain: true }));
+    let heroList = heroRoster.map((x) => x.get({
+      plain: true
+    }));
     console.log(heroList);
     res.render('roster', {
       heroList,
