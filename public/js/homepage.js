02a57f9a484ba2b453ea949bd77969956928bc3e
Justin Pickens <justin.pickens@gmx.com>
2021-08-18 21:34:12 -0500

    General cleanup and polish

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,36 +1,6 @@
-// BUTTON TO SHOW UP WHEN NO HERO ASSIGNED. THIS WILL SEND UPDATE TO SERVER FOR HERO ASSIGNED
-const joinMission = async (event) => {
-  event.preventDefault();
-  console.log(event)
-  // const join_mission = document.querySelector('#join_mission').value;
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-    // const hero = req.session.user;
-    const response = await fetch(`/api/missions/heroassign/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        id,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      document.location.replace(`/`);
-    } else {
-      document.location.replace(`/login`);
-    }
-  }
-}
-
-
-
-
-
-
 const printCSV = async (event) => {
   event.preventDefault();
-  console.log(event)
+
     const response = await fetch(`/api/data/missions`, {
       method: 'GET',
       headers: {
@@ -45,14 +15,7 @@ const printCSV = async (event) => {
   
 }
 
-const joinBtn = document.querySelectorAll('.join_mission');
-console.log(joinBtn);
-if (joinBtn[0]) {
-  [...joinBtn].map(btn => btn.addEventListener('click', joinMission));
-}
-
 const printBtn = document.querySelectorAll('.print');
-console.log(printBtn);
 if (printBtn) {
   [...printBtn].map(btn => btn.addEventListener('click', printCSV));
 }
